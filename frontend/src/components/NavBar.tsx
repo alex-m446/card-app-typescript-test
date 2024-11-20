e06029191740f51f4238a259a646ffe6eff32688
Alex <alexander.mihailiuc@gmail.com>
2024-11-20 21:53:47 +0000

    more small changes

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,24 +2,6 @@ import {NavLink} from 'react-router-dom'
 import { useState, createContext } from 'react';
 
 
-//const [isDarkMode, setDarkMode] = useState(false);
-//const bgClass = isDarkMode ? 'bg-theme-bgDark hover:bg-theme-bgDarkHover' : 'bg-theme-bg hover:bg-theme-bgHover';
-
-/*
-interface DarkModeProps {
-  isDarkMode: boolean;
-  setDarkMode: (value: boolean) => void;
-}
-
-
-const toggleDarkMode = ({ isDarkMode, setDarkMode }: DarkModeProps): void => {
-  setDarkMode(!isDarkMode);
-  document.body.style.backgroundColor = 'grey';
-  if(isDarkMode) {
-    document.body.style.backgroundColor = '';
-  };
-};
-  */
 
 export default function NavBar(props : {isDarkMode: boolean; toggleDarkMode: any}){
   const buttonClass = props.isDarkMode ? 'bg-theme-buttonDark hover:bg-theme-buttonDarkHover' : 'bg-theme-button hover:bg-theme-buttonHover';
